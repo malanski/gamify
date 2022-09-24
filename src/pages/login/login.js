60e362289f462d0d0f790df31fe397d6f171af1b
Ulisses Malanski <malanskiwork@gamil.com>
2022-09-24 03:59:54 -0300

    remove unused vars

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -47,24 +47,18 @@ const Responsive = styled("div")(({ theme }) => ({
 }))
 export function Login() {
     const [values, setValues] = React.useState({
-        amount: '',
         password: '',
-        weight: '',
-        weightRange: '',
         showPassword: false,
     });
-
     const handleChange = (prop) => (event) => {
         setValues({ ...values, [prop]: event.target.value });
     };
-
     const handleClickShowPassword = () => {
         setValues({
             ...values,
             showPassword: !values.showPassword,
         });
     };
-
     const handleMouseDownPassword = (event) => {
         event.preventDefault();
     };
